42b58f7172e67149e975d49787cd790ba5a22d66
ViktorWallin <49311375+ViktorWallin@users.noreply.github.com>
2020-04-22 10:59:44 +0200

    added build folder to assimp

--- Spelprojekt/Spelprojekt/Scenes/DevScene.cpp
+++ Spelprojekt/Spelprojekt/Scenes/DevScene.cpp
@@ -82,8 +82,8 @@ DevScene::DevScene(Renderer* renderer, DX11Handler& dx11, Window& window) : Scen
 	cube->GetTransform().Translate(15, 5, 10);
 	objects.push_back(cube);*/
 
-	/*Object* glasse = AssimpHandler::loadFbxObject("Models/Glasse.fbx", dx11.GetDevice(), dx11, defaultShader);
-	objects.push_back(glasse);*/
+	Object* glasse = AssimpHandler::loadFbxObject("Models/Glasse.fbx", dx11.GetDevice(), dx11, defaultShader);
+	objects.push_back(glasse);
 
 }
 
