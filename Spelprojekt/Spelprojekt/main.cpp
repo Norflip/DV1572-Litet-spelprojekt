f325f43c6fd40678f814c4f2504ed953a8fc8b39
Norflip <Norgren.filip@gmail.com>
2020-05-22 13:17:30 +0200

    Update main.cpp

--- Spelprojekt/Spelprojekt/main.cpp
+++ Spelprojekt/Spelprojekt/main.cpp
@@ -1,7 +1,20 @@
+#include <reactphysics3d\reactphysics3d.h>
+
+void TestPhysicis();
+
+void TestPhysicis()
+{
+	reactphysics3d::PhysicsCommon common;
+	rp3d::PhysicsWorld* world = common.createPhysicsWorld();
+
+	// YADDA YADDA
+}
 
 #include "Application.h"
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
+	TestPhysicis();
+
 	Application application (hInstance); //sets up an application with hInstance
 	application.Run(); //Starts the rendering loop
 	return 0;
